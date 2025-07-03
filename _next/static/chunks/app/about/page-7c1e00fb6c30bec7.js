62a416b35f149205cb1062ad91957e7cea528118
unknown <krishnendurenjit@gmail.com>
2025-07-03 10:55:39 +0900

    Updates

---
+++ _next/static/chunks/app/about/page-7c1e00fb6c30bec7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{130:()=>{},9182:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,130,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[171,441,684,358],()=>s(9182)),_N_E=e.O()}]);
