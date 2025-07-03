3284ef4ab48a5d43a166eb950c44b617db221216
unknown <krishnendurenjit@gmail.com>
2025-07-03 10:26:49 +0900

    bootstrap script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></script>
+      </head>
+
       <body className='text-white'>
         <Navbar />
         <main className="container mt-5">{children}</main>
